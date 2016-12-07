9acd9871ad7ceb8270535dcd6012e9e2986b2000
Ruslan Ardashev <ruslan.ardashev@us.ibm.com>
2016-12-07 13:56:26 -0500

    removes deprecated run method

--- chaincode/marbles_chaincode.go
+++ chaincode/marbles_chaincode.go
@@ -135,13 +135,6 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	return nil, nil
 }
 
-// ============================================================================================================================
-// Run - Our entry point for Invocations - [LEGACY] obc-peer 4/25/2016
-// ============================================================================================================================
-func (t *SimpleChaincode) Run(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("run is running " + function)
-	return t.Invoke(stub, function, args)
-}
 
 // ============================================================================================================================
 // Invoke - Our entry point for Invocations
