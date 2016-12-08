2c45b8d15e4a510fd28f52fce40019540d843f93
Christine Baghdassarian <croot@ca.ibm.com>
2016-12-07 16:24:27 -0800

    Removed marbles code from registries files

--- utils/ws_registries.js
+++ utils/ws_registries.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
 
-
 // ==================================
-// Part 2 - incoming messages, look for type
+// Registries - incoming messages, look for type
 // ==================================
 var ibc = {};
 var chaincode = {};
@@ -17,7 +16,7 @@ module.exports.process_msg = function(ws, data){
 	// Registry Code
 	if (data.type == 'register') {
 		console.log('[ws info] Register', data);
-		chaincode.invoke.register([data.timestamp, data.jurisdiction, data.name, data.number, data.directorName, data.address, data.email, data.date, data.status], cb_invoked);
+		chaincode.invoke.register([data.timestamp, data.jurisdiction, data.name, data.number, data.directorName, data.address, data.email, data.date, data.status], cb_register);
 	}
 	else if (data.type == 'nameChange') {
 		console.log('[ws info] Name Change', data);
@@ -31,130 +30,14 @@ module.exports.process_msg = function(ws, data){
 	}
 	else if (data.type == 'get_transactions') {
 		console.log("Get Transactions from ws_registries");
-		// TODO To be replaced with callback similar to cb_got_trades
+		// TODO To be replaced with callback similar to cb_got_trades or blockstat
 		cb_got_transactions();
-		//chaincode.query.readAll(['_corporationIndex'], cb_got_transactions);
+		
 	}
 	else if (data.type == 'get_corporations') {
 		console.log("Get corporations from ws_registries");
 		chaincode.query.readAll(['_corporationIndex'], cb_got_corporations);
 	}
-	
-	// Marbles code
-	/*if(data.v === 2){																						//only look at messages for part 2
-		if(data.type == 'create'){
-			console.log('its a create!');
-			if(data.name && data.color && data.size && data.user){
-				chaincode.invoke.init_marble([data.name, data.color, data.size, data.user], cb_invoked);	//create a new marble
-			}
-		}
-		else if(data.type == 'get'){
-			console.log('get marbles msg');
-			chaincode.query.read(['_marbleindex'], cb_got_index);
-		}
-		else if(data.type == 'transfer'){
-			console.log('transfering msg');
-			if(data.name && data.user){
-				chaincode.invoke.set_user([data.name, data.user]);
-			}
-		}
-		else if(data.type == 'remove'){
-			console.log('removing msg');
-			if(data.name){
-				chaincode.invoke.delete([data.name]);
-			}
-		}
-		else if(data.type == 'chainstats'){
-			console.log('chainstats msg');
-			ibc.chain_stats(cb_chainstats);
-		}
-		else if(data.type == 'open_trade'){
-			console.log('open_trade msg');
-			if(!data.willing || data.willing.length < 0){
-				console.log('error, "willing" is empty');
-			}
-			else if(!data.want){
-				console.log('error, "want" is empty');
-			}
-			else{
-				var args = [data.user, data.want.color, data.want.size];
-				for(var i in data.willing){
-					args.push(data.willing[i].color);
-					args.push(data.willing[i].size);
-				}
-				chaincode.invoke.open_trade(args);
-			}
-		}
-		else if(data.type == 'get_open_trades'){
-			console.log('get open trades msg');
-			chaincode.query.read(['_opentrades'], cb_got_trades);
-		}
-		else if(data.type == 'perform_trade'){
-			console.log('perform trade msg');
-			chaincode.invoke.perform_trade([data.id, data.closer.user, data.closer.name, data.opener.user, data.opener.color, data.opener.size]);
-		}
-		else if(data.type == 'remove_trade'){
-			console.log('remove trade msg');
-			chaincode.invoke.remove_trade([data.id]);
-		}
-	}*/
-	
-	
-	//got the marble index, lets get each marble
-	function cb_got_index(e, index){
-		if(e != null) console.log('[ws error] did not get marble index:', e);
-		else{
-			try{
-				var json = JSON.parse(index);
-				for(var i in json){
-					console.log('!', i, json[i]);
-					chaincode.query.read([json[i]], cb_got_marble);												//iter over each, read their values
-				}
-			}
-			catch(e){
-				console.log('[ws error] could not parse response', e);
-			}
-		}
-	}
-	
-	//call back for getting a marble, lets send a message
-	function cb_got_marble(e, marble){
-		if(e != null) console.log('[ws error] did not get marble:', e);
-		else {
-			try{
-				sendMsg({msg: 'marbles', marble: JSON.parse(marble)});
-			}
-			catch(e){}
-		}
-	}
-	
-	function cb_invoked(e, a){
-		console.log('response from blockchain: ', e, a);
-		try{
-			sendMsg({msg: 'register', status: 'OK'});
-		}
-		catch(e){
-			console.log('[ws error]', e);
-		}
-		//chaincode.query.readAll(['_corporationIndex'], cb_ctForTimestamp);
-	}
-	
-	/*function cb_ctForTimestamp(e, ctForTimestamp) {
-		if(e != null) console.log('[ws error] did not get corporations', e);
-		else{
-			console.log('[ws info]  data: ',ctForTimestamp);
-			try{
-				var json = JSON.parse(ctForTimestamp) 
-				for(var i in json){
-					console.log('!', i, json[i]);
-				}
-			}
-			catch(e){
-				console.log('[ws error] could not parse response', e);
-			}
-		}
-		
-	}*/
 	
 	//call back for getting the blockchain stats, lets get the block stats now
 	function cb_chainstats(e, chain_stats){
@@ -179,17 +62,14 @@ module.exports.process_msg = function(ws, data){
 		}
 	}
 	
-	//call back for getting open trades, lets send the trades
-	function cb_got_trades(e, trades){
-		if(e != null) console.log('[ws error] did not get open trades:', e);
-		else {
-			try{
-				trades = JSON.parse(trades);
-				if(trades && trades.open_trades){
-					sendMsg({msg: 'open_trades', open_trades: trades.open_trades});
-				}
-			}
-			catch(e){}
+	
+	function cb_register(e, a){
+		console.log('response from blockchain: ', e, a);
+		try{
+			sendMsg({msg: 'register', status: 'OK'});
+		}
+		catch(e){
+			console.log('[ws error]', e);
 		}
 	}
 
@@ -211,10 +91,6 @@ module.exports.process_msg = function(ws, data){
 			console.log('[ws info]  corporation data: ',corporations);
 			try{
 				var json = JSON.parse(corporations) 
-				for(var i in json){
-					console.log('!', i, json[i]);
-					console.log('name', i, json[i].name);
-				}
 				var message = {
 						msg: 'corporations', 
 						corporations: json};
@@ -224,27 +100,11 @@ module.exports.process_msg = function(ws, data){
 				console.log('[ws error] could not parse response', e);
 			}
 		}
-
 	}
-
 	
 	// Registry Code
 	//call back for getting transactions
-	function cb_got_transactions(e, transactions){
-		/*if(e != null) console.log('[ws error] did not get corporations', e);
-		else{
-			console.log('[ws info]  data: ',transactions);
-			try{
-				var json = JSON.parse(transactions) 
-				for(var i in json){
-					console.log('!', i, json[i]);
-				}
-			}
-			catch(e){
-				console.log('[ws error] could not parse response', e);
-			}
-		}*/
-		
+	function cb_got_transactions(e, transactions){		
 		if(e != null) console.log('[ws error] did not get transactions:', e);
 		else {
 			console.log("cb_got_transactions");
