404b6d9be5ee4ca3241c8a212c3781600dec2df3
Christine Baghdassarian <croot@ca.ibm.com>
2016-12-13 11:23:27 -0800

    Filter by jurisdiction checkboxes

--- public/js/registries_viewer.js
+++ public/js/registries_viewer.js
@@ -8,19 +8,19 @@ var delimiterlength = 0;
 // On Load
 // =================================================================================
 $(document).on('ready', function() {
-	connect_to_server();
+    connect_to_server();
 
-	// load the list of corporations
-	$('#corporationsLink').click(function(){
-		// Get the corporations on webpage load
-		ws.send(JSON.stringify({type: 'get_corporations'}));
-	});
-    
-    	// load the list of transactions
-	$('#transactionsLink').click(function(){
-		// Get the corporations on webpage load
-		ws.send(JSON.stringify({type: 'get_transactions'}));
-	});
+    // load the list of corporations
+    $('#corporationsLink').click(function(){
+        // Get the corporations on webpage load
+        ws.send(JSON.stringify({type: 'get_corporations'}));
+    });
+
+    // load the list of transactions
+    $('#transactionsLink').click(function(){
+        // Get the corporations on webpage load
+        ws.send(JSON.stringify({type: 'get_transactions'}));
+    });
 
 });
 
@@ -28,73 +28,73 @@ $(document).on('ready', function() {
 // Socket Stuff
 // =================================================================================
 function connect_to_server(){
-	var connected = false;
-	connect();
-		
-	function connect(){
-		var wsUri = 'ws://' + document.location.hostname + ':' + document.location.port;
-		console.log('Connectiong to websocket', wsUri);
-		
-		ws = new WebSocket(wsUri);
-		ws.onopen = function(evt) { onOpen(evt); };
-		ws.onclose = function(evt) { onClose(evt); };
-		ws.onmessage = function(evt) { onMessage(evt); };
-		ws.onerror = function(evt) { onError(evt); };
-	}
-	
-	function onOpen(evt){
-		console.log('WS CONNECTED');
-		connected = true;
-		clear_blocks();
-		$('#errorNotificationPanel').fadeOut();
-		
-		// Get chain stats
-		ws.send(JSON.stringify({type: 'chainstats', v:2}));
-		
-		// Registry Code
-		// Get the transactions on webpage load
-		ws.send(JSON.stringify({type: 'get_transactions'}));
-	
-		// Get the corporations on webpage load
-		ws.send(JSON.stringify({type: 'get_corporations'}));
-	}
+    var connected = false;
+    connect();
 
-	function onClose(evt){
-		console.log('WS DISCONNECTED', evt);
-		connected = false;
-		setTimeout(function(){ connect(); }, 5000);					//try again one more time, server restarts are quick
-	}
+    function connect(){
+        var wsUri = 'ws://' + document.location.hostname + ':' + document.location.port;
+        console.log('Connectiong to websocket', wsUri);
 
-	function onMessage(msg){
-		// Registry Code
-		try {
-			var msgObj = JSON.parse(msg.data);
-			if(msgObj.msg === 'transactions'){
-				console.log('transactions from register_viewer', msgObj.msg, msgObj);
-				build_transactions(msgObj.transactions);
-			}
-			else if(msgObj.msg === 'corporations'){
-				console.log('corporations', msgObj.msg, msgObj);
-				build_corporations(msgObj.corporations);
-				corporations = msgObj.corporations;
-			}
-		}
-		catch(e){
-			console.log('ERROR', e);
-			//ws.close();
-		}	
-	}
+        ws = new WebSocket(wsUri);
+        ws.onopen = function(evt) { onOpen(evt); };
+        ws.onclose = function(evt) { onClose(evt); };
+        ws.onmessage = function(evt) { onMessage(evt); };
+        ws.onerror = function(evt) { onError(evt); };
+    }
 
-	function onError(evt){
-		console.log('ERROR ', evt);
-		if(!connected && bag.e == null){											//don't overwrite an error message
-			$('#errorName').html('Warning');
-			$('#errorNoticeText').html('Waiting on the node server to open up so we can talk to the blockchain. ');
-			$('#errorNoticeText').append('This app is likely still starting up. ');
-			$('#errorNoticeText').append('Check the server logs if this message does not go away in 1 minute. ');
-			$('#errorNotificationPanel').fadeIn();
-		}
-	}
+    function onOpen(evt){
+        console.log('WS CONNECTED');
+        connected = true;
+        clear_blocks();
+        $('#errorNotificationPanel').fadeOut();
+
+        // Get chain stats
+        ws.send(JSON.stringify({type: 'chainstats', v:2}));
+
+        // Registry Code
+        // Get the transactions on webpage load
+        ws.send(JSON.stringify({type: 'get_transactions'}));
+
+        // Get the corporations on webpage load
+        ws.send(JSON.stringify({type: 'get_corporations'}));
+    }
+
+    function onClose(evt){
+        console.log('WS DISCONNECTED', evt);
+        connected = false;
+        setTimeout(function(){ connect(); }, 5000);					//try again one more time, server restarts are quick
+    }
+
+    function onMessage(msg){
+        // Registry Code
+        try {
+            var msgObj = JSON.parse(msg.data);
+            if(msgObj.msg === 'transactions'){
+                console.log('transactions from register_viewer', msgObj.msg, msgObj);
+                build_transactions(msgObj.transactions);
+            }
+            else if(msgObj.msg === 'corporations'){
+                console.log('corporations', msgObj.msg, msgObj);
+                build_corporations(msgObj.corporations);
+                corporations = msgObj.corporations;
+            }
+        }
+        catch(e){
+            console.log('ERROR', e);
+            //ws.close();
+        }	
+    }
+
+    function onError(evt){
+        console.log('ERROR ', evt);
+        if(!connected && bag.e == null){											//don't overwrite an error message
+            $('#errorName').html('Warning');
+            $('#errorNoticeText').html('Waiting on the node server to open up so we can talk to the blockchain. ');
+            $('#errorNoticeText').append('This app is likely still starting up. ');
+            $('#errorNoticeText').append('Check the server logs if this message does not go away in 1 minute. ');
+            $('#errorNotificationPanel').fadeIn();
+        }
+    }
 }
 
 
@@ -104,14 +104,14 @@ function connect_to_server(){
 // Registry Code
 
 function build_transactions(transactions){
-	console.log('Building transactions table');
-	var html = '';
-	
-	
-	for(var i in transactions){
-		console.log(transactions[i]);
-		var data = '';
-        
+    console.log('Building transactions table');
+    var html = '';
+
+
+    for(var i in transactions){
+        console.log(transactions[i]);
+        var data = '';
+
         if (transactions[i].corporationNumber) {
             data += '' + transactions[i].corporationNumber + ' ';
         }
@@ -133,123 +133,156 @@ function build_transactions(transactions){
         if (transactions[i].type == 'Name Change') {
             //data +='' + transactions
         }
-        
-		var style = ' ';
-			html += '<tr class="' + style + '">';
-			html +=		'<td>' + transactions[i].corporationName + '</td>';
-			html +=		'<td>' + transactions[i].jurisdiction + '</td>';
-			html +=		'<td>' + transactions[i].transactionType + '</td>';
-			html +=		'<td>' + data + '</td>';
-			html +=		'<td>' + transactions[i].block + '</td>';
-            html +=		'<td>' + transactions[i].timestamp + '</td>';
-			html +=		'<td></td>';
-			html += '</tr>';
-	}
-	if(html === '') html = '<tr><td>nothing here...</td><td></td><td></td><td></td><td></td><td></td></tr>';
-    
+
+        var style = ' ';
+        html += '<tr class="' + style + '">';
+        html +=		'<td>' + transactions[i].corporationName + '</td>';
+        html +=		'<td>' + transactions[i].jurisdiction + '</td>';
+        html +=		'<td>' + transactions[i].transactionType + '</td>';
+        html +=		'<td>' + data + '</td>';
+        html +=		'<td>' + transactions[i].block + '</td>';
+        html +=		'<td>' + transactions[i].timestamp + '</td>';
+        html +=		'<td></td>';
+        html += '</tr>';
+    }
+    if(html === '') html = '<tr><td>nothing here...</td><td></td><td></td><td></td><td></td><td></td></tr>';
+
     $('#myTransactionsBody').append(html);
     //$('#myTransactionsBody').prepend(html);
 }
 
 function build_corporations(corporations){
-	console.log('Building corporation table');
-	var html = '';
-	for(var i in corporations){
-		console.log(corporations[i].name.substring(0,delimiterlength) + " " + corporations[i].number.substring(0,delimiterlength));
-		var style = ' ';
-			html += '<tr class="' + style + '">';
-			html +=		'<td>' + corporations[i].name.substring(0,corporations[i].name.length - delimiterlength) + '</td>';
-			html +=		'<td>' + corporations[i].number.substring(0,corporations[i].number.length - delimiterlength) + '</td>';
-			html +=		'<td>' + corporations[i].date.substring(0,corporations[i].date.length - delimiterlength) + '</td>';
-			html +=		'<td>' + corporations[i].jurisdiction.substring(0,corporations[i].jurisdiction.length - delimiterlength) + '</td>';
-			html +=		'<td>' + corporations[i].directorName.substring(0,corporations[i].directorName.length - delimiterlength) + '</td>';
-			html +=		'<td>' + corporations[i].email.substring(0,corporations[i].email.length - delimiterlength) + '</td>';
-			html +=		'<td>' + corporations[i].address.substring(0,corporations[i].address.length - delimiterlength) + '</td>';
-			html +=		'<td>' + corporations[i].status.substring(0,corporations[i].status.length - delimiterlength) + '</td>';
-			html +=		'<td></td>';
-			html += '</tr>';
-	}
-	if(html === '') html = '<tr><td>nothing here...</td><td></td><td></td><td></td><td></td><td></td></tr>';
-	$('#corporationsBody').html(html);
+    console.log('Building corporation table');
+    var html = '';
+    for(var i in corporations){
+        console.log(corporations[i].name.substring(0,delimiterlength) + " " + corporations[i].number.substring(0,delimiterlength));
+        var style = ' ';
+        html += '<tr class="' + style + '">';
+        html +=		'<td>' + corporations[i].name.substring(0,corporations[i].name.length - delimiterlength) + '</td>';
+        html +=		'<td>' + corporations[i].number.substring(0,corporations[i].number.length - delimiterlength) + '</td>';
+        html +=		'<td>' + corporations[i].date.substring(0,corporations[i].date.length - delimiterlength) + '</td>';
+        html +=		'<td>' + corporations[i].jurisdiction.substring(0,corporations[i].jurisdiction.length - delimiterlength) + '</td>';
+        html +=		'<td>' + corporations[i].directorName.substring(0,corporations[i].directorName.length - delimiterlength) + '</td>';
+        html +=		'<td>' + corporations[i].email.substring(0,corporations[i].email.length - delimiterlength) + '</td>';
+        html +=		'<td>' + corporations[i].address.substring(0,corporations[i].address.length - delimiterlength) + '</td>';
+        html +=		'<td>' + corporations[i].status.substring(0,corporations[i].status.length - delimiterlength) + '</td>';
+        html +=		'<td></td>';
+        html += '</tr>';
+    }
+    if(html === '') html = '<tr><td>nothing here...</td><td></td><td></td><td></td><td></td><td></td></tr>';
+    $('#corporationsBody').html(html);
 }
 
 
 function filterTransactionsByCorporation() {
-	  console.log("Filter the results!!");
-	  // Declare variables
-	  var input, filter, table, tr, td, i;
-	  input = document.getElementById("corporateNameTransactionsFilter");
-	  filter = input.value.toUpperCase();
-	  table = document.getElementById("registryTransactionsTable");
-	  tr = table.getElementsByTagName("tr");
+    console.log("Filter the results!!");
+    // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("corporateNameTransactionsFilter");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("registryTransactionsTable");
+    tr = table.getElementsByTagName("tr");
 
-	  // Loop through all table rows, and hide those who don't match the search query
-	  for (i = 0; i < tr.length; i++) {
-	    td = tr[i].getElementsByTagName("td")[0];
-	    if (td) {
-	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-	        tr[i].style.display = "";
-	      } else {
-	        tr[i].style.display = "none";
-	      }
-	    }
-	  }
-	}
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
 
 function filterCorporationsByName() {
-	  console.log("Filter by corporationName");
-	  // Declare variables
-	  var input, filter, table, tr, td, i;
-	  input = document.getElementById("corporateNameFilter");
-	  filter = input.value.toUpperCase();
-	  table = document.getElementById("corporationsTable");
-	  tr = table.getElementsByTagName("tr");
+    console.log("Filter by corporationName");
+    // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("corporateNameFilter");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("corporationsTable");
+    tr = table.getElementsByTagName("tr");
 
-	  // Loop through all table rows, and hide those who don't match the search query
-	  for (i = 0; i < tr.length; i++) {
-	    td = tr[i].getElementsByTagName("td")[0];
-	    if (td) {
-	      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-	        tr[i].style.display = "";
-	      } else {
-	        tr[i].style.display = "none";
-	      }
-	    }
-	  }
-	}
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
 
-function filterTransactionsByProvince() {
-	console.log('Filter by jurisdiction!');
-	var input, filter, table, tr, td, i;
-	  //input = document.getElementById("myInput");
-	  //filter = input.value.toUpperCase();
-	  table = document.getElementById("registryTransactionsTable");
-	  tr = table.getElementsByTagName("tr");
-	  
-	  var filter = $('select[name="jurisdictionFilter"]').val();
-	  
-	  console.log('Filter on ' + filter);
-	  
-	  if (filter == 'ALL') {
-		  // Show all
-		  for (i = 0; i < tr.length; i++) {
-			  td = tr[i].getElementsByTagName("td")[3];
-			  	if (td) {
-			        tr[i].style.display = "";
-			    }
-			  }
-	  }
-	  else {
-		  for (i = 0; i < tr.length; i++) {
-		    td = tr[i].getElementsByTagName("td")[3];
-		    if (td) {
-		    	console.log(td.innerHTML);
-		      if (td.innerHTML.indexOf(filter) > -1) {
-		        tr[i].style.display = "";
-		      } else {
-		        tr[i].style.display = "none";
-		      }
-		    }
-		  }
-	  }
+var jurisdictions = [];
+
+function buildJurisdictionFilter(jurisdiction) {
+    if (document.getElementById(jurisdiction).checked) {
+        //console.log(jurisdiction + " is checked");
+        jurisdictions.push(jurisdiction);
+    }
+    else {
+        //console.log(jurisdiction + " is unchecked");
+        var index = jurisdictions.indexOf(jurisdiction);
+        if (index > -1) {
+            jurisdictions.splice(index, 1);
+        }
+    }
+}
+
+function filterTransactionsByJurisdiction() {
+    console.log('Filter by jurisdiction!');
+    var input, table, tr, td, i;
+
+    table = document.getElementById("registryTransactionsTable");
+    tr = table.getElementsByTagName("tr");
+
+    jurisdictions = [];
+
+    buildJurisdictionFilter('AB');
+    buildJurisdictionFilter('BC');
+    buildJurisdictionFilter('MB');
+    buildJurisdictionFilter('NB');
+    buildJurisdictionFilter('NL');
+    buildJurisdictionFilter('NT');
+    buildJurisdictionFilter('NS');
+    buildJurisdictionFilter('NU');
+    buildJurisdictionFilter('ON');
+    buildJurisdictionFilter('PE');
+    buildJurisdictionFilter('QC');
+    buildJurisdictionFilter('SK');
+    buildJurisdictionFilter('YT');
+    buildJurisdictionFilter('FED');
+
+    console.log('Jurisdiction Count:' + jurisdictions.length);
+    // if the array is empty show all
+    if (jurisdictions.length === 0) {
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+                tr[i].style.display = "";
+            }
+        }
+    }
+    else {
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+                // iterate over jurisdictions to display and check
+                for (index = 0; index < jurisdictions.length; ++index) {
+
+                    if (td.innerHTML.indexOf(jurisdictions[index]) > -1) {
+                        tr[i].style.display = "";
+                        break;  // once found break out of here
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    }
 }
